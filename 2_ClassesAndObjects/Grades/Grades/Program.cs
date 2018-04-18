da16606dae703f879badd0538fc7acc0eb2d6613
neous1 <Neous1@users.noreply.github.com>
2018-04-18 17:42:09 -0400

    gradebook program

--- 2_ClassesAndObjects/Grades/Grades/Program.cs
+++ 2_ClassesAndObjects/Grades/Grades/Program.cs
@@ -4,12 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Grades
 {
+    class GradeBooks
+    {
+        //HOld State adn Behavior
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        //use list from the collection to store grades
+
+        List<float> grades = new List<float>(); // use the paren becz a new2 list object is constructed.
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            GradeBooks book=new GradeBooks(); //define a variable book of type Gradbooks
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
         }
     }
+
 }
