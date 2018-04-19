a5fb1e0ced4fbfa385c779c92228fa7208c15a6f
neous1 <Neous1@users.noreply.github.com>
2018-04-19 18:27:52 -0400

    refactored program.cs

--- 2_ClassesAndObjects/Grades/Grades/Program.cs
+++ 2_ClassesAndObjects/Grades/Grades/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Speech.Synthesis;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,9 +12,7 @@ namespace Grades
 
         static void Main(string[] args)
         {
-
-           SpeechSynthesizer synth = new SpeechSynthesizer();
-            synth.Speak("Hello! This is Yvon's grade book program");
+            
 
             GradeBooks book = new GradeBooks(); //define a variable book of type Gradbooks
             book.AddGrade(91);
@@ -29,7 +26,7 @@ namespace Grades
             Console.WriteLine("Lowest Grade: " +  stats.LowestGrades);
 
 
-
+            
             
         }
     }
