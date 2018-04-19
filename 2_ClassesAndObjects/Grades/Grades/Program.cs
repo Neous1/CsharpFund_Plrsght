d5936d8b03f72bbc5cb6e4663f55d8a1ac807320
neous1 <Neous1@users.noreply.github.com>
2018-04-19 05:54:20 -0400

    Call computeStatistic from program.cs and update assembly

--- 2_ClassesAndObjects/Grades/Grades/Program.cs
+++ 2_ClassesAndObjects/Grades/Grades/Program.cs
@@ -9,14 +9,19 @@ namespace Grades
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            GradeBooks book=new GradeBooks(); //define a variable book of type Gradbooks
+            GradeBooks book = new GradeBooks(); //define a variable book of type Gradbooks
             book.AddGrade(91);
             book.AddGrade(89.5f);
 
+            GradeStatistics stats = book.CommputeStatistics();
+
             GradeBooks book2 = book;
             book2.AddGrade(75);
+
+            
         }
     }
 
