75718cd8dd9c2c155c02f0717ce0edf00ddc0823
neous1 <Neous1@users.noreply.github.com>
2018-04-19 08:29:45 -0400

    Add variables and a default constructor to GradeStatistics

--- 2_ClassesAndObjects/Grades/Grades/GradeStatistics.cs
+++ 2_ClassesAndObjects/Grades/Grades/GradeStatistics.cs
@@ -2,6 +2,14 @@
 {
     public class GradeStatistics
     {
-            
+
+        public GradeStatistics()
+        {
+            HighestGrade = 0;
+            LowestGrades = float.MaxValue;
+        }
+        public float AverageGrade;
+        public float HighestGrade;
+        public float LowestGrades;
     }
 }
