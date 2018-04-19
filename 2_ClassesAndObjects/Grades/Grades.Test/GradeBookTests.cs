5fa449e56a46f99e6d305ce000d5daacfb626386
neous1 <Neous1@users.noreply.github.com>
2018-04-19 18:59:46 -0400

    Add tests for computeLowestGrade() and ComputeAverageGrade()

--- 2_ClassesAndObjects/Grades/Grades.Test/GradeBookTests.cs
+++ 2_ClassesAndObjects/Grades/Grades.Test/GradeBookTests.cs
@@ -20,5 +20,29 @@ namespace Grades.Test
             GradeStatistics result = book.ComputeStatistics();
             Assert.AreEqual(90, result.HighestGrade);
         }
+
+        [TestMethod]
+        public void ComputeLowestGrade()
+        {
+            GradeBooks book = new GradeBooks();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(10, result.LowestGrades);
+        }
+
+        [TestMethod]
+        public void ComputeAverageGrade()
+        {
+            GradeBooks book = new GradeBooks();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+
+            GradeStatistics result = book.ComputeStatistics();
+            Assert.AreEqual(85.16, result.AverageGrade,0.01);
+        }
     }
 }
