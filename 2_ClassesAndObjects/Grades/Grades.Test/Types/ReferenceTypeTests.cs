9db3d8078b6a73d5c060b24eb0011e9c1b56e199
neous1 <Neous1@users.noreply.github.com>
2018-04-20 22:32:24 -0400

    Create ReferenceTypeTest class

---
+++ 2_ClassesAndObjects/Grades/Grades.Test/Types/ReferenceTypeTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grades.Test.Types
+{
+    [TestClass]
+    public class ReferenceTypeTests
+    {
+        [TestMethod]
+        public void VariableHoldAReference()
+        {
+            GradeBooks g1 = new GradeBooks();
+            GradeBooks g2 = g1;
+
+            g1.Name = "Yvon's grade book";
+            Assert.AreEqual(g1.Name, g2.Name);
+        
+        }
+    }
+}
