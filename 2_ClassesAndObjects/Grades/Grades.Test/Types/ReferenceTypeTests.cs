2758089daf64c11215b68fc7a64926891f0480d8
neous1 <Neous1@users.noreply.github.com>
2018-04-20 23:00:08 -0400

    Add Value test

--- 2_ClassesAndObjects/Grades/Grades.Test/Types/ReferenceTypeTests.cs
+++ 2_ClassesAndObjects/Grades/Grades.Test/Types/ReferenceTypeTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -11,10 +12,21 @@ namespace Grades.Test.Types
     public class ReferenceTypeTests
     {
         [TestMethod]
+        public void IntVariablesHoldAValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+
+            x1 = 4;
+            Assert.AreNotEqual(x1, x2);
+        } 
+        [TestMethod]
         public void VariableHoldAReference()
         {
             GradeBooks g1 = new GradeBooks();
             GradeBooks g2 = g1;
+
+            g1 = new GradeBooks();
 
             g1.Name = "Yvon's grade book";
             Assert.AreEqual(g1.Name, g2.Name);
