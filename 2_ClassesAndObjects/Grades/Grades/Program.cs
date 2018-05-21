0d03f2d025200ec84683b1822455b18b26967d91
Neous1 <Neous1@users.noreply.github.com>
2018-05-21 18:44:32 -0400

    Refactor codes in program.cs

--- 2_ClassesAndObjects/Grades/Grades/Program.cs
+++ 2_ClassesAndObjects/Grades/Grades/Program.cs
@@ -16,6 +16,42 @@ namespace Grades
         {
 
             GradeBook book = new GradeBook();
+            GetBookName(book);
+
+            AddGrades(book);
+
+            SaveGrades(book);
+
+            WriteResults(book);
+        }
+
+        private static void WriteResults(GradeBook book)
+        {
+            GradeStatistics stats = book.ComputeStatistics();
+            WriteResult("Average Grade", stats.AverageGrade);
+            WriteResult("Highest Grade", stats.HighestGrade); //stats highestGrade gets coerces into type int
+            WriteResult("Lowest Grade", stats.LowestGrades);
+            WriteResult(stats.Description, stats.LetterGrade);
+        }
+
+        private static void SaveGrades(GradeBook book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+
+            }
+        }
+
+        private static void AddGrades(GradeBook book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+        }
+
+        private static void GetBookName(GradeBook book)
+        {
             try
             {
                 Console.WriteLine("Enter a name");
@@ -24,28 +60,8 @@ namespace Grades
             catch (ArgumentException ex)
             {
                 Console.WriteLine(ex.Message);
-                
             }
-            book.AddGrade(91);
-            book.AddGrade(89.5f);
-            book.AddGrade(75);
-
-            using (StreamWriter outputFile = File.CreateText("grades.txt"))
-            {
-                book.WriteGrades(outputFile);
-                
-            }
-
-            GradeStatistics stats = book.ComputeStatistics();
-
-            Console.WriteLine(book.Name);
-            WriteResult("Average Grade" , stats.AverageGrade);
-            WriteResult("Highest Grade" , stats.HighestGrade); //stats highestGrade gets coerces into type int
-            WriteResult("Lowest Grade" , stats.LowestGrades);
-            WriteResult(stats.Description, stats.LetterGrade);
         }
-
-
 
         static void WriteResult(string description, string result)
         {
