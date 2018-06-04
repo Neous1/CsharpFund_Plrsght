eca3616cb0e9a41063d13d3f43990e79a24bb1dd
Neous1 <Neous1@users.noreply.github.com>
2018-06-04 19:18:27 -0400

    Add GetEnumerator() to GradeBook

--- 2_ClassesAndObjects/Grades/Grades/GradeBook.cs
+++ 2_ClassesAndObjects/Grades/Grades/GradeBook.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -38,9 +39,10 @@ namespace Grades
             grades.Add(grade);
         }
 
-
-
-
+        public override IEnumerator GetEnumerator()
+        {
+            return grades.GetEnumerator();
+        }
 
         protected List<float> grades;
 
